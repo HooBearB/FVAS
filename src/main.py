766fec28820dcfba6e88464e41bb5705a698858f
HooBearB <nbeeson708891@gmail.com>
2022-11-16 15:39:31 -0600

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       Kenny                                                        #
+# 	Created:      11/16/2022, 3:38:49 PM                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
